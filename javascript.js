3b64c5681c43107d3b5645a38f4fee0115b940bd
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-01-13 10:10:54 +0100

    deleted comments

--- javascript.js
+++ javascript.js
@@ -80,29 +80,6 @@ function game() {
     }
 }
 
+
+//calling function to start the game
 game();
-
-
-
-
-/*
-
-function getPlayerChoice() {
-    let validatedInput = false;
-    while(validatedInput == false) {
-        const choice = prompt("Rock Paper Scissors");
-        if (choice == null) {
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-        if (options.includes(choiceInLower)) {
-            validatedInput = true;
-            return choiceInLower;
-        }
-    }
-}
-*/
-
-
-
-
