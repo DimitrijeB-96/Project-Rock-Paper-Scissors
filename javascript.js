3f60598e3bf90b4782dea5444bf9f51289458c39
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-03-11 16:56:29 +0100

    added basic css for styling

--- javascript.js
+++ javascript.js
@@ -7,24 +7,47 @@ const scissors = document.getElementById('scissors');
 const roundCounter = document.querySelector('.round-counter');
 const playerScore = document.querySelector('.player-score');
 const computerScore = document.querySelector('.computer-score');
+const roundWinner = document.querySelector('.round-winner');
+
 const checkWinner = document.querySelector('.score-container');
+const continuePlayingMessage = document.querySelector('.continue-playing');
 const displayWinner = document.querySelector('.winner');
+
+
+const displayPlayerChoice = document.querySelector('.player-choice');
+const displayComputerChoice = document.querySelector('.computer-choice');
+
 
 const resetBtn = document.querySelector('.reset');
 resetBtn.addEventListener('click', () => {
     rock.disabled = false;
+    rock.style.backgroundColor = '#111';
+    rock.style.cursor = 'pointer';
+
     paper.disabled = false;
+    paper.style.backgroundColor = '#111';
+    paper.style.cursor = 'pointer';
+
     scissors.disabled = false;
+    scissors.style.backgroundColor = '#111';
+    scissors.style.cursor = 'pointer';
 
     roundCounter.textContent = 0;
     playerScore.textContent = 0;
     computerScore.textContent = 0;
 
+    displayPlayerChoice.textContent = '';
+    displayComputerChoice.textContent = '';
+    roundWinner.textContent = '';
+
+    continuePlayingMessage.textContent = '';
     displayWinner.textContent = '';
+
 });
 
 function computerChoice() {
     const random = Math.floor(Math.random() * choices.length);
+    displayComputerChoice.textContent = ` ${choices[random]}`;
     return choices[random];
 }
 
@@ -35,13 +58,22 @@ playerSelection.forEach(btn => btn.addEventListener('click', (e) => {
     playRound(player, computerChoice());
     if ((playerScore.textContent == '5') || (computerScore.textContent == '5')) {
         rock.disabled = true;
+        rock.style.backgroundColor = 'gray';
+        rock.style.cursor = 'unset';
+
         paper.disabled = true;
+        paper.style.backgroundColor = 'gray';
+        paper.style.cursor = 'unset';
+
         scissors.disabled = true;
+        scissors.style.backgroundColor = 'gray';
+        scissors.style.cursor = 'unset';
     }
 }));
 
 function getPlayerChoice(playerChoice) {
     let playerResult = playerChoice;
+    displayPlayerChoice.textContent = ` ${playerResult}`;
 
     if (playerResult === 'rock') {
         return 'rock';
@@ -53,26 +85,37 @@ function getPlayerChoice(playerChoice) {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playRoundWinner;
+    let playRoundWinner = roundWinner;
 
     console.log(`Player: ${playerSelection}`);
     console.log(`Computer: ${computerSelection}`);
 
     if(playerSelection === computerSelection) {
-        playRoundWinner = 'tie'; //not used
+        playRoundWinner.textContent = "It's a tie!";
+        playRoundWinner.style.color = 'purple';
     } else if((playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
             playerScore.textContent++;
-            playRoundWinner ='player';  //not used         
+            playRoundWinner.textContent = "You won this round"; 
+            playRoundWinner.style.color = 'green';          
     } else {
             computerScore.textContent++;
-            playRoundWinner = 'computer'; //not used
+            playRoundWinner.textContent = "Computer won this round";
+            playRoundWinner.style.color = 'red';
     }   
 }
 
 function gameOver(gameWinner) {
-    displayWinner.textContent = `${gameWinner}`;
+    continuePlayingMessage.textContent = 'Click Reset button to play again.'; 
+    if (gameWinner == 'Player') {
+        displayWinner.textContent = 'Congratulations you are the winner!';
+        displayWinner.style.color = 'green';
+    } else {
+        displayWinner.textContent = 'Better luck next time, Computer won this match.';
+        displayWinner.style.color = 'red';
+    }
+     
 }
 
 checkWinner.addEventListener('DOMSubtreeModified', () => {
