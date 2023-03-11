b8c811d34080bb09f88bf438e24f958f47d510d3
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-03-11 15:40:34 +0100

    Game logic completed

--- javascript.js
+++ javascript.js
@@ -1,70 +1,93 @@
 const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice() {
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+const roundCounter = document.querySelector('.round-counter');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+const checkWinner = document.querySelector('.score-container');
+const displayWinner = document.querySelector('.winner');
+
+const resetBtn = document.querySelector('.reset');
+resetBtn.addEventListener('click', () => {
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+
+    roundCounter.textContent = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+
+    displayWinner.textContent = '';
+});
+
+function computerChoice() {
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
 
+const playerSelection = document.querySelectorAll('.btn');
+playerSelection.forEach(btn => btn.addEventListener('click', (e) => {
+    roundCounter.textContent++;
+    const player = getPlayerChoice(e.target.id); 
+    playRound(player, computerChoice());
+    if ((playerScore.textContent == '5') || (computerScore.textContent == '5')) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    }
+}));
 
-function getPlayerChoice() {
-    const playerSelection = prompt('Your choice is ?\nRock, Paper or Scissors?');
-    if (playerSelection.toLowerCase() === 'rock') {
+function getPlayerChoice(playerChoice) {
+    let playerResult = playerChoice;
+
+    if (playerResult === 'rock') {
         return 'rock';
-    } else if (playerSelection.toLowerCase() === 'paper') {
+    } else if (playerResult === 'paper') {
         return 'paper';
-    } else if (playerSelection.toLocaleLowerCase() === 'scissors') {
+    } else if (playerResult === 'scissors') {
         return 'scissors';
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
-    playerSelection  = getPlayerChoice();
+    let playRoundWinner;
 
-    let getWinner;
     console.log(`Player: ${playerSelection}`);
     console.log(`Computer: ${computerSelection}`);
 
-
     if(playerSelection === computerSelection) {
-        return getWinner = 'tie';
+        playRoundWinner = 'tie'; //not used
     } else if((playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            return getWinner = 'player';
+            playerScore.textContent++;
+            playRoundWinner ='player';  //not used         
     } else {
-            return getWinner = 'computer';
-    }
+            computerScore.textContent++;
+            playRoundWinner = 'computer'; //not used
+    }   
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for(let i = 0; i < 5; i++) {
-        const winner = playRound(getPlayerChoice, getComputerChoice);
-        
-        if (winner === 'player') {
-            console.log(` >You won!`);
-            playerScore++;
-        } else if (winner === 'computer') {
-            console.log(` >Computer won!`);
-            computerScore++;
-        } else {
-            console.log(` >It's a tie!`);
-        }
-    }
-    console.log(`\nComputer score: ${computerScore}`);
-    console.log(`Player score: ${playerScore}`);
-    if (playerScore > computerScore) {
-        console.log('You are the match winner!')
-    } else if (computerScore > playerScore) {
-        console.log('Computer is the match winner');
-    } else {
-        console.log("It's a tie!");
-    }
+function gameOver(gameWinner) {
+    displayWinner.textContent = `${gameWinner}`;
 }
 
-game();
+checkWinner.addEventListener('DOMSubtreeModified', () => {
+    let gameWinner;
+
+    if (playerScore.textContent == '5') {
+        gameWinner = 'Player';
+        gameOver(gameWinner);
+    }else if (computerScore.textContent == '5') {
+        gameWinner = 'Computer';
+        gameOver(gameWinner);
+    }
+});
+
+
+
 
 
